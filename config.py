b678781005b093849eeb86211c90585ba0f7ec1f
Elyse Kriegel <55773029+elyskrie21@users.noreply.github.com>
2020-12-06 20:43:13 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 keys = {
   'product_url':'https://www.newegg.com/Product/ComboDealDetails?ItemList=Combo.4208042',
-  'email': 'nenentro.gaming@gmail.com',
-  'password': '@ill77moN',
-  'card': '5131158652707719',
-  'first': 'Elyse',
-  'last': 'Kriegel',
-  'address':'960 202nd Ave',
-  'city': 'Pella',
-  'zip': '50219',
-  'code': '337'
+  'email': '',
+  'password': '',
+  'card': '',
+  'first': '',
+  'last': '',
+  'address':'',
+  'city': '',
+  'zip': '',
+  'code': ''
 }
