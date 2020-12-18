c166d67fc866c7edcfe6fa8e8f210c83e900583d
Elyse Kriegel <55773029+elyskrie21@users.noreply.github.com>
2020-12-18 10:51:13 -0600

    Update sendEmail.py

--- sendEmail.py
+++ sendEmail.py
@@ -2,16 +2,16 @@ import smtplib, ssl
 from email.message import EmailMessage
 
 msg = EmailMessage()
-msg.set_content("It worked")
+msg.set_content("Your_Message")
 msg["Subject"] = "Graphics Card"
-msg["From"] = "esamkriegel@gmail.com"
-msg["To"] = "elyskrie21@pellaschools.org"
+msg["From"] = "Your_Email"
+msg["To"] = ""
 
 context=ssl.create_default_context()
 
 with smtplib.SMTP("smtp.google.com", port=28) as smtp:
     smtp.starttls(context=context)
-    smtp.login(msg["From"], "ill77mon")
+    smtp.login(msg["From"], "Password_Goes_Here")
     smtp.send_message(msg)
 
 server.quit()
